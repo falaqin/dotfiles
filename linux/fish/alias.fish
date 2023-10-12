8484fb0d56ab20514d349d6fb9a3539548c192e4
falaqin <falaqin.work@gmail.com>
2023-10-12 17:07:25 +0800

    update alias

--- linux/fish/alias.fish
+++ linux/fish/alias.fish
@@ -1,6 +1,9 @@
 alias lg=lazygit
 alias phpchange="sudo update-alternatives --config php"
-alias neofetch="/usr/bin/neofetch"
 alias pa="php artisan"
 alias pv="php -v"
 alias sa="sudo apt"
+
+# WSL
+alias neofetch="/usr/bin/neofetch"
+alias open="explorer.exe ."
