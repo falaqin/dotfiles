e5892239b5dc1843d0f14af346a87b468a95e8b7
falaqin <falaqin.work@gmail.com>
2023-10-31 16:02:08 +0800

    -

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -3,4 +3,7 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 	eval "$(oh-my-posh init fish --config ~/.poshthemes/catppuccin.omp.json)"
+
+	 # Add composer path
+        fish_add_path ".config/composer/vendor/bin"
 end
